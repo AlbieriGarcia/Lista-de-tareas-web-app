6fc96fa5e1a57ae7a744336c94d6b18ba37f54ce
AlbieriGarcia <albieri404@gmail.com>
2024-05-07 14:49:48 -0400

    First commit - CRUD

---
+++ Server/ListaDeTareas/Controllers/TareasController.cs
@@ -0,0 +1,65 @@
+﻿using ListaDeTareas.Data.Repositories;
+using ListaDeTareas.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeTareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly ITareaRepository _tareaRepository;
+
+        public TareasController(ITareaRepository tareaRepository)
+        {
+            _tareaRepository = tareaRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTareas()
+        {
+            return Ok(await _tareaRepository.GetAllTareas());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTareaById(int id)
+        {
+            return Ok(await _tareaRepository.GetTareaById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTarea([FromBody] Tarea tarea)
+        {
+            if (tarea == null)
+                return BadRequest();
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var created = await _tareaRepository.InsertTarea(tarea);
+
+            return Created("created", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTarea([FromBody] Tarea tarea)
+        {
+            if (tarea == null)
+                return BadRequest();
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            await _tareaRepository.UpdateTarea(tarea);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTarea(int id)
+        {
+            await _tareaRepository.DeleteTarea(new Tarea { Id = id });
+
+            return NoContent();
+        }
+    }
+}

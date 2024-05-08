9d9eb1a7d9272d284c2bf88f37d37c520c873024
AlbieriGarcia <albieri404@gmail.com>
2024-05-08 15:06:48 -0400

    Implementacion obtener tareas de la API

--- Client/app.js
+++ Client/app.js
@@ -15,3 +15,35 @@ btnCerrarForm.addEventListener("click",()=>{
     modal.close()
 })
 
+//Obtener tareas
+document.addEventListener('DOMContentLoaded', function() {
+    // Llamada a la función para cargar los datos del JSON
+    cargarDatos();
+});
+
+async function cargarDatos() {
+
+    const response = await fetch('https://localhost:7058/api/Tareas');
+
+    const datos = await response.json();
+
+    console.log(datos)
+        
+    const datosDiv = document.getElementById('lista');
+    datosDiv.innerHTML = ''; 
+
+    datos.forEach(dato => {
+        // Crear un nuevo div para cada dato
+        const datoDiv = document.createElement('div');
+        datoDiv.innerHTML = `
+            <li>
+                <i class="far fa-circle co" data="Completado" id="0"></i>
+                <p class="text">${dato.title}</p>
+                <i class="fas fa-trash de" data="eliminado" id="0"></i>
+            </li>
+        `;
+        // Agregar el nuevo div al contenedor de la lista
+        datosDiv.appendChild(datoDiv);
+    });
+       
+}

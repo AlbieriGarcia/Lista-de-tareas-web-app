45aa905140d7f92367adfcc43127a1ab71f076b4
AlbieriGarcia <albieri404@gmail.com>
2024-05-09 18:25:47 -0400

    Base de datos-update

--- DataBase/lista_de_tareas_tareas.sql
+++ DataBase/lista_de_tareas_tareas.sql
@@ -1,56 +1,20 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
---
--- Host: localhost    Database: lista_de_tareas
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.27-MariaDB
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS lista_de_tareas;
+USE lista_de_tareas;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Creación de la tabla "tareas"
+CREATE TABLE IF NOT EXISTS tareas (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(500),
+  date DATE NOT NULL,
+  time TIME,
+  category varchar(45) NOT NULL,
+  state varchar(1) NOT NULL
+);
 
---
--- Table structure for table `tareas`
---
-
-DROP TABLE IF EXISTS `tareas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tareas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `date` varchar(30) NOT NULL,
-  `time` varchar(8) DEFAULT NULL,
-  `category` varchar(45) NOT NULL,
-  `state` varchar(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tareas`
---
-
-LOCK TABLES `tareas` WRITE;
-/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
-INSERT INTO `tareas` VALUES (106,'Estudiar Programacion Movil','Estudiar y investigar las diferentes tecnologias para el desarrollo movil.','2024-05-10','09:30','1','1'),(107,'Estudiar los principios SOLID','Investigar sobre los principios SOLID, como se implementan y cuales son sus ventajas.','2024-05-12','17:00','1','1'),(108,'Comprar pizza para cenar','','2024-05-09','16:24','3','2');
-/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-05-09 16:27:22
+-- Inserción de datos en la tabla "tareas"
+INSERT INTO tareas (title, description, date, time, category, state) VALUES 
+('Estudiar Programacion Movil', 'Estudiar y investigar las diferentes tecnologias para el desarrollo movil.', '2024-05-10', '09:30', 'Personal', '1'),
+('Estudiar los principios SOLID', 'Investigar sobre los principios SOLID, como se implementan y cuales son sus ventajas.', '2024-05-12', '17:00', 'Personal', '1'),
+('Comprar pizza para cenar', '', '2024-05-09', '16:24', 'Compras', '2');
